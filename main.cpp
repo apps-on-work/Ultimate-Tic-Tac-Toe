35846436ab7ccc6dfa78159cdf5b5aa3cb338b87
apps-on-work <apoorvyash129@gmail.com>
2026-06-11 12:04:16 +0530

    made board

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
-
+#define ll long long
+using namespace std;
+char board[3][3];
+void clearboard(){
+    for(ll i{};i<3;++i){
+        for(ll j{};j<3;++j){
+            board[i][j]=' ';
+        }
+    }
+}
+void rintboard(){
+    cout<<"---------"<<endl;
+    for(ll i{};i<3;++i){
+        cout<<" "<<board[i][0]<<" "<<"|"<<" "<<board[i][1]<<" "<<"|"<<" "<<board[i][2]<<" "<<endl;
+        cout<<"---------"<<endl;
+    }
+}
 int main()
 {
-    std::cout << "Tic Tac Toe\n";
+    cout << "Tic Tac Toe\n";
+    clearboard();
+    rintboard();
     return 0;
 }
